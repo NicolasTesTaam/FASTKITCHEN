f20dcd6ff4d2f0fda4a651af6795771f95c7dc03
DuccTaam <mytaam2005mt@gmail.com>
2025-11-12 16:37:44 +0700

    #Cập nhật

---
+++ js/Navbar.js
@@ -0,0 +1,9 @@
+const HomeLink= document.getElementById('Home');
+const ServiceLink= document.getElementById('Service');
+HomeLink.addEventListener('click', function() {
+    window.location.href = 'TrangChu.html';
+});
+
+ServiceLink.addEventListener('click', function() {
+    window.location.href = 'TrangDVCaNhan.html';
+});
